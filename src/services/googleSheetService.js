684350ed99a8849f5bc3d28de479232316a06ad3
DDJ-02 <149932599+DDJ-02@users.noreply.github.com>
2025-12-10 16:43:35 +0530

    Update googleSheetService.js

--- src/services/googleSheetService.js
+++ src/services/googleSheetService.js
@@ -39,10 +39,16 @@ async function getSheetData(range = 'Sheet1!A:D') {
   try {
     // Create GoogleAuth instance
     console.log('🔑 Check THIS Authenticating with Google Sheets API...')  ;
-    const auth = new google.auth.GoogleAuth({
-      keyFile: process.env.GOOGLE_CREDENTIALS_PATH,// path.join(__dirname, 'google-credentials.json'), // <- your JSON key
-      scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'],
-    });
+    try{
+      
+      const auth = new google.auth.GoogleAuth({
+        keyFile: process.env.GOOGLE_CREDENTIALS_PATH,// path.join(__dirname, 'google-credentials.json'), // <- your JSON key
+        scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'],
+      });
+
+    }catch(err){
+      console.log('aaaaaaaaaaaaaa in google auth creation:', err.message);
+    }
     console.log('Got authentocated');
 
     try{
